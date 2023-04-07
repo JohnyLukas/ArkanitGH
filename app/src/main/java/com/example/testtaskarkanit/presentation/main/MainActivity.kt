7cd2e158b1984f3d2b1876cb15cee5cf91a73b7e
Evgenii <evgenij.luk11@gmail.com>
2023-04-07 20:17:09 +0700

    setup

--- app/src/main/java/com/example/testtaskarkanit/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/testtaskarkanit/presentation/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.testtaskarkanit
+package com.example.testtaskarkanit.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testtaskarkanit.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
